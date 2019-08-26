b32682779bb75eb3f5472c9bbd3407c17f1a961a
Kumano Shuta <shoota.dev@gmail.com>
2019-08-26 22:57:30 +0900

    configure storybook

---
+++ .storybook/config.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { addParameters } from '@storybook/react'
+
+import '../src/index.less'
+
+const req = require.context('../src/components', true, /.stories.tsx?$/)
+const loadStories = () => req.keys().forEach(filename => req(filename))
+
+addDecorator(withKnobs)
+
+addParameters({
+  viewports: {
+    responsive: {
+      name: 'Responsive',
+      styles: { width: '100%', height: '100%' },
+    },
+    xxl: {
+      name: 'xxl',
+      styles: { width: '1600px', height: '100%' },
+    },
+    xl: {
+      name: 'xl',
+      styles: { width: '1200px', height: '100%' },
+    },
+    lg: {
+      name: 'lg',
+      styles: { width: '992px', height: '100%' },
+    },
+    md: {
+      name: 'md',
+      styles: { width: '768px', height: '100%' },
+    },
+    sm: {
+      name: 'sm',
+      styles: { width: '576px', height: '100%' },
+    },
+    xs: {
+      name: 'xs',
+      styles: { width: '568px', height: '100%' },
+    },
+  },
+})
+
+configure(loadStories, module)
