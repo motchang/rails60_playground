6b04c1a51dbcc489e21636b3156f65d1f79f4ed5
Kumano Shuta <shoota.dev@gmail.com>
2019-08-27 10:27:00 +0900

    remove viewport params

--- .storybook/config.js
+++ .storybook/config.js
@@ -10,37 +10,4 @@ const loadStories = () => req.keys().forEach(filename => req(filename))
 
 addDecorator(withKnobs)
 
-addParameters({
-  viewports: {
-    responsive: {
-      name: 'Responsive',
-      styles: { width: '100%', height: '100%' },
-    },
-    xxl: {
-      name: 'xxl',
-      styles: { width: '1600px', height: '100%' },
-    },
-    xl: {
-      name: 'xl',
-      styles: { width: '1200px', height: '100%' },
-    },
-    lg: {
-      name: 'lg',
-      styles: { width: '992px', height: '100%' },
-    },
-    md: {
-      name: 'md',
-      styles: { width: '768px', height: '100%' },
-    },
-    sm: {
-      name: 'sm',
-      styles: { width: '576px', height: '100%' },
-    },
-    xs: {
-      name: 'xs',
-      styles: { width: '568px', height: '100%' },
-    },
-  },
-})
-
 configure(loadStories, module)
