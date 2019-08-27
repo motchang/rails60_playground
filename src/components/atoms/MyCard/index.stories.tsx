d5b9e863396880950f81053f339e41aa42e3cb0d
Kumano Shuta <shoota.dev@gmail.com>
2019-08-27 10:33:36 +0900

    use knob

--- src/components/atoms/MyCard/index.stories.tsx
+++ src/components/atoms/MyCard/index.stories.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
+import { boolean, text } from '@storybook/addon-knobs'
 import { lorem } from 'faker'
 
 import MyCard from '.'
 
 storiesOf('atoms/MyCard', module).add('default', () => (
-  <MyCard isLoading={false} contents={lorem.paragraphs()} />
+  <MyCard
+    isLoading={boolean('isLoading', false)}
+    contents={text('contents', lorem.paragraphs())}
+  />
 ))
