337a45feccbdcfed088660be970410ae824efb4d
Kumano Shuta <shoota.dev@gmail.com>
2019-08-28 15:20:53 +0900

    add tests

--- e2e/index.ts
+++ e2e/index.ts
@@ -1,9 +1,32 @@
 import { Selector } from 'testcafe'
 
-fixture('top').page('http://todomvc.com/examples/react/#/')
+const url = 'http://todomvc.com/examples/react/#/'
+const selectorInput = '.new-todo'
 
+fixture('top').page(url)
 test('onload', async t => {
   await t
-    .expect(Selector('.new-todo').getAttribute('placeholder'))
+    .expect(Selector(selectorInput).getAttribute('placeholder'))
     .eql('What needs to be done?')
+})
+
+test('add', async t => {
+  const todoList = Selector('.todo-list .view')
+  const completeButton = Selector('.filters li')
+    .sibling(-1)
+    .find('a')
+
+  await t
+    .typeText(selectorInput, 'buy tomato')
+    .pressKey('enter')
+    // clear text
+    .expect(Selector(selectorInput).value)
+    .eql('')
+    // add list
+    .expect(todoList.count)
+    .eql(1)
+    // filter completed
+    .click(completeButton)
+    .expect(todoList.count)
+    .eql(0)
 })
