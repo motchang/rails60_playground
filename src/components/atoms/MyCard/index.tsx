6ee134252048a0b523a452315e76f8b320b86e4d
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2019-08-30 14:59:14 +0900

    refactor: apply emotion

--- src/components/atoms/MyCard/index.tsx
+++ src/components/atoms/MyCard/index.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react'
-import { Card as AntCard } from 'antd'
+import { Card } from 'antd'
+import { CardProps } from 'antd/lib/card'
+import styled from '@emotion/styled'
 
 export type Props = {
   isLoading: boolean
   contents: string
 }
 
+type StyledCardProps = CardProps & {
+  bgColor?: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const StyledCard = styled<React.FC<StyledCardProps>>(Card as any)`
+  width: 500px;
+  margin: 10px;
+  background: ${props => props.bgColor};
+`
+
 const MyCard: React.FC<Props> = ({ isLoading, contents }) => (
-  <AntCard
-    loading={isLoading}
-    title="title"
-    style={{ width: 500, margin: '10px' }}
-  >
+  <StyledCard loading={isLoading} title="title">
     {contents}
-  </AntCard>
+  </StyledCard>
 )
 
 export default MyCard
