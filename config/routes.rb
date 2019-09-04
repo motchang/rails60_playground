217254dad6b17b9c2b8937b20e7071f4cae1a299
motchang <motchang@gmail.com>
2019-09-04 15:20:36 +0900

    Apply cop, change definition order

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+  post '/graphql', to: 'graphql#execute'
+
   devise_for :users
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
-  post "/graphql", to: "graphql#execute"
   root to: 'main#index'
 end
