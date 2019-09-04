1ad3033a3eb66fae721514408a699c8c3eff01b5
motchang <motchang@gmail.com>
2019-09-04 18:22:22 +0900

    Change some style, Apply styles

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class Types::CommentType < Types::BaseObject
-  field :id, ID, null: false
-  field :post, Types::PostType, null: false
-  field :description, String, null: false
+module Types
+  class CommentType < Types::BaseObject
+    field :id, ID, null: false
+    field :post, Types::PostType, null: false
+    field :description, String, null: false
+  end
 end
