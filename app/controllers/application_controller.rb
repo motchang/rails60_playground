e1d8b5982d0866a1de84dd77785aa6dd74cf6dca
motchang <motchang@gmail.com>
2019-09-04 15:42:28 +0900

    Detect current_site in before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # https://github.com/ErwinM/acts_as_tenant#setting-the-current-tenant-in-a-controller-manually
+  set_current_tenant_through_filter
+  before_action :detect_current_site
+
+  private
+
+  def detect_current_site
+    current_site = Site.find_by!(fqdn: request.env['SERVER_NAME'])
+    Rails.logger.debug("current_site: #{current_site}")
+    set_current_tenant(current_site)
+  end
 end
